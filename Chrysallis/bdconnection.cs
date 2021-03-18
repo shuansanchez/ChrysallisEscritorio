1bc2292a4cb1e6e412d985acdff1073984a0bbe9
cepPorritas <visualstudio.cep@hotmail.com>
2021-03-18 12:31:40 +0100

    Añadido la BD

---
+++ Chrysallis/bdconnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chrysallis
+{
+    class bdconnection
+    {
+        public static SqlConnection connection =
+            new SqlConnection("Data Source=localhost\\sqlexpress;DATABASE=chrysallis;User ID=sa; Password=Informatica_2020;");
+    }
+}
