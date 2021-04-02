e344b3c01a172161bc135497897d025fcfce97f0
cepPorritas <visualstudio.cep@hotmail.com>
2021-04-02 12:35:23 +0200

    Cambiado un boton de CreadModEventos

--- Chrysallis/Editar_Localidades.Designer.cs
+++ Chrysallis/Editar_Localidades.Designer.cs
@@ -28,13 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Editar_Localidades));
             this.SuspendLayout();
             // 
             // Editar_Localidades
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Editar_Localidades";
             this.Text = "Editar_Localidades";
             this.Load += new System.EventHandler(this.Editar_Localidades_Load);
