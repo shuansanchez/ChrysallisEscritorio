ee56c94f096902f8ea0bb86337f2d9a8b5fce3e1
cepPorritas <visualstudio.cep@hotmail.com>
2021-04-02 18:37:10 +0200

    Solo faltra filtro en Editar_Localidades

--- Chrysallis/Editar_Localidades.cs
+++ Chrysallis/Editar_Localidades.cs
@@ -12,6 +12,8 @@ namespace Chrysallis
 {
     public partial class Editar_Localidades : Form
     {
+       
+
         public Editar_Localidades()
         {
             InitializeComponent();
@@ -19,13 +21,42 @@ namespace Chrysallis
 
         private void Editar_Localidades_Load(object sender, EventArgs e)
         {
-
+           
+            dataGridViewLocalidades.DataSource = ConsultaOrm.SelectMostrarLoc();
         }
 
         private void toolStripButton2_Click(object sender, EventArgs e)
         {
             AñadirLocalidad localidadForm = new AñadirLocalidad();
             localidadForm.ShowDialog();
+            dataGridViewLocalidades.DataSource = ConsultaOrm.SelectMostrarLoc();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewLocalidades.SelectedRows.Count > 0)
+            {
+                BindingList<localitats> localidades = new BindingList<localitats>(ConsultaOrm.SelectMostrarLoc());
+                DialogResult dialogConfirmaBorra = MessageBox.Show("¿Estás seguro de borrar?", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (dialogConfirmaBorra == DialogResult.OK)
+                {
+
+                    //aqui borramos 
+                    localitats borrar = (localitats)dataGridViewLocalidades.CurrentRow.DataBoundItem;
+                    ConsultaOrm.DeleteLocalidad(borrar);
+                    dataGridViewLocalidades.DataSource = ConsultaOrm.SelectMostrarLoc();
+                }
+            }
+        }
+
+        private void Editar_Localidades_Activated(object sender, EventArgs e)
+        {
+            
         }
     }
 }
